48cb18ee4bd24e0ccdffc259d67bcbfe6c0a8d83
Marcel Reis <marcelclemente.msc@gmail.com>
2022-08-14 11:07:16 +0200

    fire animation and mouse tracking

--- js/main.js
+++ js/main.js
@@ -1,42 +1,78 @@
 "use strict";
 /*
 TODO: 8bit style circle at the center
-DONE: circle around mouse being tracked
 TODO: particles flowing up from fire source
 */
 class Fire {
     constructor(canvas) {
         this.canvas = canvas;
-        this.ctx = this.canvas.getContext("2d");
-        this.setupCanvas();
-        this.size = 10;
+        this.ctx = canvas.getContext("2d");
+        this.size = 20;
+        this.mouse_x = 150;
+        this.mouse_y = 150;
+        this.currentFrame = 0;
+        this.particles = [];
+        this.init();
     }
-    setupCanvas() {
+    init() {
         this.canvas.width = window.innerWidth;
         this.canvas.height = window.innerHeight;
         this.cv_width = this.canvas.width;
         this.cv_height = this.canvas.height;
-        this.mouseTracking();
+        this.mouseTracker();
+        requestAnimationFrame(() => this.render());
     }
-    mouseTracking() {
+    mouseTracker() {
         this.canvas.addEventListener("mousemove", (event) => {
-            const mouse_x = event.x;
-            const mouse_y = event.y;
-            this.clearCanvas();
-            this.renderSource(mouse_x, mouse_y);
+            this.mouse_x = event.x;
+            this.mouse_y = event.y;
+            // this.clearCanvas();
+            // this.renderSource(mouse_x, mouse_y);
         });
     }
     clearCanvas() {
-        var _a;
-        (_a = this.ctx) === null || _a === void 0 ? void 0 : _a.clearRect(0, 0, this.cv_width, this.cv_height);
+        this.ctx.clearRect(0, 0, this.cv_width, this.cv_height);
     }
-    renderSource(mouse_x, mouse_y) {
-        if (this.ctx) {
-            this.ctx.fillStyle = "cyan";
-            this.ctx.beginPath();
-            this.ctx.arc(mouse_x, mouse_y, this.size, 0, 2 * Math.PI);
-            this.ctx.fill();
+    render() {
+        this.clearCanvas();
+        if (this.currentFrame >= 60) {
+            this.currentFrame = 0;
+            // this.particles = [];
         }
+        //draw source
+        this.ctx.fillStyle = "orange";
+        this.ctx.beginPath();
+        this.ctx.arc(this.mouse_x, this.mouse_y, this.size, 0, 2 * Math.PI);
+        this.ctx.fill();
+        // draw particles
+        if (this.particles.length < 100) {
+            this.particles.push(new Particle(this.mouse_x, this.mouse_y, this.size));
+        }
+        else {
+            this.particles.shift();
+            this.particles.push(new Particle(this.mouse_x, this.mouse_y, this.size));
+        }
+        this.particles.forEach((particle) => particle.render(this.currentFrame, this.ctx));
+        // this.drawParticle(this.mouse_x, this.mouse_y);
+        this.currentFrame += 1;
+        requestAnimationFrame(() => this.render());
+    }
+}
+class Particle {
+    constructor(init_x, init_y, init_size = 20) {
+        this.init_x = init_x + 3 * Math.random();
+        this.init_y = init_y;
+        this.init_time = new Date().getTime();
+        this.init_size = init_size;
+    }
+    render(currentFrame, ctx) {
+        const currTime = new Date().getTime();
+        const frame = ((currTime - this.init_time) / 1000) * 60;
+        ctx.fillStyle = `rgb(250,${250 - (frame / 60) * 250},0,${1 - frame / 60})`;
+        ctx.beginPath();
+        const randomNumber = Math.random();
+        ctx.arc(this.init_x + Math.sin((frame / 4) * randomNumber), this.init_y - (frame * this.init_size) / 7, this.init_size / (frame / 20 + 1), 0, 2 * Math.PI);
+        ctx.fill();
     }
 }
 const canvas = document.getElementById("view");
